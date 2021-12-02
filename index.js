c67d6639e945ab1a423cd0adeb4479d6cc922d88
MalayP98 <tomalaypandey.tanu@gmail.com>
2021-12-03 00:44:37 +0530

    headers addded

--- index.js
+++ index.js
@@ -25,13 +25,48 @@ function rgb(red, green, blue) {
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
+var configTabItems = document.getElementsByClassName("nav-item");
+var configAreas = ["authorization", "headers", "body", "params"];
+currentConfigTabIdx = 0;
+
+function selectConfig(idx) {
+    configTabItems[currentConfigTabIdx].style.backgroundColor = rgb(206, 203, 209);
+    configTabItems[currentConfigTabIdx].style.color = "white";
+    document.getElementById(configAreas[currentConfigTabIdx]).style.display = "none";
+    currentConfigTabIdx = idx;
+    document.getElementById(configAreas[currentConfigTabIdx]).style.display = "flex";
+    currentConfigTab = configTabItems[currentConfigTabIdx];
+    currentConfigTab.style.backgroundColor = rgb(142, 197, 236);
+    currentConfigTab.style.color = rgb(0, 0, 0);
+}
+
 // default active tab will be Authorization
-var defaultConfigTab = document.getElementsByClassName("nav-item")[0]
-defaultConfigTab.style.backgroundColor = rgb(142, 197, 236);
-defaultConfigTab.style.color = rgb(0, 0, 0);
-console.log(document.getElementsByClassName("nav-item")[0]);
+selectConfig(currentConfigTabIdx);
 
 // default auth type tab
-// var defaultAuthTypeTab = document.getElementsByClassName
+function activeAuthType(authType) {
+    authType.style.backgroundColor = rgb(142, 197, 236);
+    authType.style.color = "white";
+}
+
+function deactivateAuthType(authType) {
+    authType.style.backgroundColor = "white";
+    authType.style.color = rgb(142, 197, 236);
+}
+
+var authTypeList = ["apikey", "bearer", "basic"];
+var authTypes = document.getElementsByClassName("auth-type");
+var currentAuthType = 0;
+activeAuthType(authTypes[currentAuthType]);
+
+function selectAuthType(idx) {
+    deactivateAuthType(authTypes[currentAuthType]);
+    document.getElementsByClassName(authTypeList[currentAuthType])[0].style.display = "none"
+    currentAuthType = idx;
+    activeAuthType(authTypes[currentAuthType]);
+    document.getElementsByClassName(authTypeList[currentAuthType])[0].style.display = "flex";
+}
+
+
 
 

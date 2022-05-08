13725ab495e93b74e70cd175e5b8fb00eabc06c0
MalayP98 <tomalaypandey.tanu@gmail.com>
2022-05-08 18:30:32 +0530

    request going to backend

--- index.js
+++ index.js
@@ -72,11 +72,75 @@ function addRow(id) {
                 <div id="${id}">
                     <input placeholder="KEY" class="apikey-inp" type="text">
                     <input placeholder="VALUE" class="apikey-inp" type="text">
-                    <div class="delete-row">DELETE</div>
+                    <div class="delete-row" onclick="deleteRow(this)">DELETE</div>
                 </div>
     `;
 }
 
+function deleteRow(div) {
+    var grandParent = div.parentNode.parentNode;
+    grandParent.removeChild(div.parentNode);
+}
 
+var bodyTypes = ["form-data", "url-encoded-fields", "raw"];
+var currentBodyTypeIdx = 0;
+function selectBodyType(bodyTypeIdx) {
+    var payloadOpt = document.getElementsByClassName("payload-opt");
+    var currentBodyType = payloadOpt[currentBodyTypeIdx];
+    currentBodyType.style.backgroundColor = "white";
+    currentBodyType.style.color = rgb(142, 197, 236);
+    document.getElementById(bodyTypes[currentBodyTypeIdx]).style.display = "none";
+    currentBodyTypeIdx = bodyTypeIdx;
+    var selectedBodyType = payloadOpt[currentBodyTypeIdx];
+    selectedBodyType.style.backgroundColor = rgb(142, 197, 236);
+    selectedBodyType.style.color = "white";
+    document.getElementById(bodyTypes[currentBodyTypeIdx]).style.display = "flex";
+}
 
+selectBodyType(0);
 
+function getRequestType() {
+    return document.getElementById("method-selector").querySelector('p').textContent;
+}
+
+function getUrl() {
+    return (document.getElementsByClassName("common-input")[0]).value;
+}
+
+function getPayload() {
+
+}
+
+function getAuth() {
+    var authType = authTypeList[currentAuthType];
+    switch (authType) {
+        case "apikey":
+            var key = (document.getElementsByClassName("apikey-inp")[0]).value;
+            var value = (document.getElementsByClassName("apikey-inp")[1]).value;
+            if (!key || !value) return ["NOAUTH", {}];
+            return ["API_KEY", { "key": key, "value": value }];
+    }
+}
+
+async function sendRequest() {
+    var BACKEND_URL = "http://localhost:8080/api/request/send/";
+    var requestType = getRequestType();
+    var url = getUrl();
+    var payload = getPayload();
+    var authentication = getAuth();
+
+    var body = { "method": requestType, "endpoint": url, "authType": authentication[0], "authroization": JSON.stringify(authentication[1]), "bodyType": "NONE" };
+
+    let formData = new FormData();
+    formData.append('body', JSON.stringify(body));
+    formData.append('files', new Blob([], { type: "text/plain;charset=utf-8" }));
+
+    const response = await fetch(BACKEND_URL, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic bWFsYXlAZ21haWwuY29tOm1hbGF5QDEyMw=='
+        },
+        body: formData
+    });
+    console.log(response.json());
+}
